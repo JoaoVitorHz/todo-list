81734be9386d5e345c79ee218dcce7a270dba664
João Vitor <vitorjoao39207@gmail.com>
2022-07-29 02:28:05 -0300

    Criação de listas, e exibindo tarefas com base nas listas

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 ?>
 
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -19,8 +19,38 @@
         <h1>Lista de tarefas</h1>
         <hr/>
 
+
+        <div class="listas">
+                <div class="inserirNovaLista">
+                    <form action="">
+                        <input type="text" name="addNameList" placeholder="Digite aqui uma nova lista">
+                        <button type="submit">Criar nova Lista</button>
+                    </form>
+                    <?php 
+                        if(!empty($_GET['addNameList'])){
+                            $nameList = $_GET['addNameList'];
+                            $tarefas->addNewList($nameList);
+                            header("Location: index.php");
+                        }
+                    ?>
+                </div>
+                <div class="ListarListas">
+                    <?php 
+                        $button = $tarefas->getAllList();
+                        foreach($button as $t):
+                    ?>
+                        <a href="index.php?id=<?php echo $t['id']?>"><?php echo $t['listName']?></a>
+                    <?php endforeach?>
+                </div>
+         
+           
+        </div>
+
         <form action="" method="">
             <div class="input">
+                      
+                        
+                <input type="hidden" name="idList" value="<?php if(!empty($_GET['id'])){ echo $idList = $_GET['id'];}?>">
                 <input type="text" name="newTask" placeholder="Digite aqui uma nova tarefas">
                 <button type="submit">Adicionar</button>
             </div>
@@ -28,17 +58,20 @@
 
             <table width="500">
                 <?php 
-                    $lista = $tarefas->getAllTask();
+                    if(!empty($_GET['id'])){
+                    $idList = $_GET['id'];
+                        
+                    $lista = $tarefas->getTaskList($idList);
                     foreach($lista as $iten):
                 ?>
                 <tr class="linha">
                     <td class="task"><?php echo $iten['taskName'];?></td>
                     <td class="actions">
-                        <a class="cheked <?php echo ($iten['taskCompleted']) ? 'feito' : 'falta'?>" href="taskCompleted.php?concluido=<?php echo $iten['taskCompleted']?>&id=<?php echo $iten['id']?>"> <?php echo ($iten['taskCompleted'] ? '<i class="icone fa-solid fa-check"></i>': '<i class="icone fa-solid fa-arrow-rotate-right"></i>')?></a>
+                        <a class="cheked <?php echo ($iten['taskCompleted']) ? 'feito' : 'falta'?>" href="taskCompleted.php?concluido=<?php echo $iten['taskCompleted']?>&id=<?php echo $iten['id']?>"> <?php echo ($iten['taskCompleted'] ? '<i class="icone fa-solid fa-arrow-rotate-right"></i>': '<i class="icone fa-solid fa-check"></i>')?></a>
                         <a class="apagar" href="excluir.php?id=<?php echo $iten['id']; ?>"><i class="icone fa-solid fa-x"></i></a>
                     </td>
                 </tr>
-                <?php endforeach;?>
+                <?php endforeach;}?>
             </table>
         </form>
     </div>
@@ -46,8 +79,9 @@
     <?php 
         if(!empty($_GET['newTask'])){
             $task = $_GET['newTask'];
-
-            $tarefas->addNewTask($task);
+            $idList = $_GET['idList'];
+            echo $idList;
+            $tarefas->addNewTask($task, $idList);
             header("Location: index.php");
         }
     ?>
--- model.php
+++ model.php
@@ -5,21 +5,37 @@ class Tarefas{
         $this->pdo = new PDO("mysql:dbname=task;host=localhost", "root", "");
     }
     //inseri novas tarefas no banco
-    public function addNewTask($task){
-        $sql = "INSERT INTO tasks(taskName) value (:task)";
+    public function addNewTask($task, $idList){
+        $sql = "INSERT INTO tasks (taskName, listId) value (:task, :listId)";
         $sql = $this->pdo->prepare($sql);
         $sql->bindValue(':task', $task);
+        $sql->bindValue(':listId', $idList);
         $sql->execute();
-        if(!empty($task)){
-            echo "Inseriu a task ".$task;
-        }
     } 
+    public function addNewList($list){
+        $sql = "INSERT INTO list(listName) value (:list)";
+        $sql = $this->pdo->prepare($sql);
+        $sql->bindValue(':list', $list);
+        $sql->execute();
+    }
 
     //pega as tarefas do banco
     public function getAllTask(){
         $sql = "SELECT * FROM tasks";
 
         $sql = $this->pdo->query($sql);
+        return $sql->fetchAll();
+    }
+    public function getAllList(){
+        $sql = "SELECT * FROM list";
+        $sql = $this->pdo->query($sql);
+        return $sql->fetchAll();
+    }
+    public function getTaskList($id){
+        $sql = "SELECT * FROM tasks WHERE listId = :id";
+        $sql = $this->pdo->prepare($sql);
+        $sql->bindValue(':id', $id);
+        $sql->execute();
         return $sql->fetchAll();
     }
 
